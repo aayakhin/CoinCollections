647732e2269f3c7b1430533ddbea85a02f0dc455
Andrey Yakhin <Andrey.yahin@gmail.com>
2023-06-27 18:39:12 +0300

    Изменение таблицы

--- src/main/java/ru/yakhin/coincollections/DAO/CoinMapper.java
+++ src/main/java/ru/yakhin/coincollections/DAO/CoinMapper.java
@@ -10,7 +10,6 @@ public class CoinMapper implements RowMapper<Coin> {
     @Override
     public Coin mapRow(ResultSet resultSet, int rowNum) throws SQLException {
         Coin coin = new Coin();
-        coin = new Coin();
         coin.setId(resultSet.getInt("id"));
         coin.setDenomination(resultSet.getString("denomination"));
         coin.setName(resultSet.getString("name"));
