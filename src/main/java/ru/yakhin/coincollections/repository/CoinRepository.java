c72f5e6ecebc2f7cab2a792d16bacd54ead33c73
Andrey Yakhin <Andrey.yahin@gmail.com>
2023-07-02 16:35:53 +0300

    Pagination and Search

--- src/main/java/ru/yakhin/coincollections/repository/CoinRepository.java
+++ src/main/java/ru/yakhin/coincollections/repository/CoinRepository.java
@@ -1,8 +1,11 @@
 package ru.yakhin.coincollections.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.yakhin.coincollections.model.Coin;
 @Repository
 public interface CoinRepository extends JpaRepository<Coin, Integer> {
+    Page<Coin> findByNameContainingIgnoreCase(String keyword, Pageable pageable);
 }
